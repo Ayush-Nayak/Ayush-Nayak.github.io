b692c0fe678ef8f2fd0564d9d77e7901844dd859
Shad0w7 <ayush.nayak@gmail.com>
2021-02-06 16:43:28 -0800

    added a ton!

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+# welcome to my website
+
+def myGithub():
+    return "link"
+
+def myRedditProfile():
+    return "link"
+
+discord = "Shad0w7#0320"
+
+about = "i am a random kid"
+print(about)
+
+achievements = []
